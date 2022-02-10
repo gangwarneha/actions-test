666389cedbc16a81a49dda1dd74d15e8aede01cb
Neha Gangwar <neha.gangwar@globant.com>
2022-02-10 15:23:57 +0530

    intial commit

---
+++ web/modules/test.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Description.
+ */
+
+$a = 10;
+$b = 20;
+$c = $a + $b;
+
+//Add addition here
+
+echo "Sum: ", $c;
+
+//Add multiplication here
+
+$c = $a * $b;
+echo "multiple:", $c;

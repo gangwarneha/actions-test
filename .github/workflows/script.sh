477aecf50569746e8cede526a88e6ed182e63ccf
Neha Gangwar <neha.gangwar@globant.com>
2022-03-21 15:30:26 +0530

    updating workflow files

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,6 +1,8 @@
+        vendor/bin/phpcbf
         retVal=$?
         if [ $retVal -eq 1 ] then
             echo "Some Files Fixed"
+            echo '::set-output name=FIXED::true'
             exit 0
         fi
         exit $retVal

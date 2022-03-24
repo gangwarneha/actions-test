8cd1fd674fbd739a86d01db988630bc283bb9b8a
gangwarneha <neha.gangwar89@gmail.com>
2022-03-24 12:57:45 +0530

    Added custom module (#3)
    * Added custom module
    
    * Fixing phpcbf changes
    
    * changes in custom module
    
    * Fixing phpcbf changes
    
    * removed unused varibles
    
    * updated new jobs in workflow
    
    Co-authored-by: Neha Gangwar <neha.gangwar@globant.com>
    Co-authored-by: gangwarneha <gangwarneha@users.noreply.github.com>

---
+++ web/modules/custom/field_demo/src/Plugin/Field/FieldFormatter/TextFieldFormatter.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\field_demo\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ * Create a new formatter for demo_text_field text field.
+ *
+ * @FieldFormatter(
+ *   id = "device_specific_formatter",
+ *   label = @Translation("Device specific formatter"),
+ *   field_types = {
+ *     "demo_text_field"
+ *   }
+ * )
+ */
+class TextFieldFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'mob' => 356,
+      'tab' => 800,
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    $summary[] = $this->t('Show text with styles definition per field');
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+
+      if (!empty($item->options['desktop'])) {
+        $desktop = $item->options['desktop'];
+      }
+
+      if (!empty($item->options['tablet'])) {
+        $tablet = $item->options['tablet'];
+      }
+
+      if (!empty($item->options['mobile'])) {
+        $mobile = $item->options['mobile'];
+      }
+
+      $elements[$delta] = [
+        '#type' => 'markup',
+        '#markup' => '<div class = "viewport-field mob ">' . $mobile . '</div><div class = "viewport-field tab ">' . $tablet . '</div><div class = "viewport-field desk">' . $desktop . '</div>',
+      ];
+
+    }
+
+    return $elements;
+  }
+
+}
---
+++ web/modules/custom/field_demo/src/Plugin/Field/FieldWidget/TextFieldWidget.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\field_demo\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'demo_text_field' widget.
+ *
+ * @FieldWidget(
+ *   id = "device_specific_widget",
+ *   label = @Translation("Device Specific Widget"),
+ *   field_types = {
+ *     "demo_text_field"
+ *   }
+ * )
+ */
+class TextFieldWidget extends WidgetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $item = $items[$delta];
+
+    $element['options']['desktop'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Desktop content'),
+      '#default_value' => !empty($item->options['desktop'])
+      ? $item->options['desktop'] : FALSE,
+    ];
+    $element['options']['tablet'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Tablet content'),
+      '#default_value' => !empty($item->options['tablet'])
+      ? $item->options['tablet'] : FALSE,
+    ];
+    $element['options']['mobile'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Mobile content'),
+      '#default_value' => !empty($item->options['mobile'])
+      ? $item->options['mobile'] : FALSE,
+    ];
+
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
+    return array_map(
+      function (array $value) {
+        if (isset($value['options'])) {
+          $value['options'] = array_filter(
+            $value['options'],
+            function ($attribute) {
+              return $attribute !== "";
+            }
+          );
+        }
+
+        return $value;
+      },
+      $values
+    );
+  }
+
+}

c6557089fc7a88d0abfdf7f51b17e583b69cd65a
Neha Gangwar <neha.gangwar@globant.com>
2022-05-27 18:31:08 +0530

    adding global services to check phpcs

--- web/modules/custom/field_demo/src/Plugin/Field/FieldFormatter/TextFieldFormatter.php
+++ web/modules/custom/field_demo/src/Plugin/Field/FieldFormatter/TextFieldFormatter.php
@@ -43,7 +43,7 @@ class TextFieldFormatter extends FormatterBase {
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
-
+    $currentUser = \Drupal::currentUser();
     foreach ($items as $delta => $item) {
 
       if (!empty($item->options['desktop'])) {
@@ -60,7 +60,7 @@ class TextFieldFormatter extends FormatterBase {
 
       $elements[$delta] = [
         '#type' => 'markup',
-        '#markup' => '<div class = "viewport-field mob ">' . $mobile . '</div><div class = "viewport-field tab ">' . $tablet . '</div><div class = "viewport-field desk">' . $desktop . '</div>',
+        '#markup' => '<div class = "userview">'. $currentUser . '</div><div class = "viewport-field mob ">' . $mobile . '</div><div class = "viewport-field tab ">' . $tablet . '</div><div class = "viewport-field desk">' . $desktop . '</div>',
       ];
 
     }

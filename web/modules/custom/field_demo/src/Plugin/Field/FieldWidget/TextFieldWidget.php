2cf7d1ed96619986c713cbb2848ef51040f9bf04
Neha Gangwar <neha.gangwar@globant.com>
2022-05-27 17:37:38 +0530

    updating files

--- web/modules/custom/field_demo/src/Plugin/Field/FieldWidget/TextFieldWidget.php
+++ web/modules/custom/field_demo/src/Plugin/Field/FieldWidget/TextFieldWidget.php
@@ -24,11 +24,10 @@ class TextFieldWidget extends WidgetBase {
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     $item = $items[$delta];
-    $stringTranslation = \Drupal::service('string_translation');
 
     $element['options']['desktop'] = [
       '#type' => 'textfield',
-      '#title' => $stringTranslation->translate('About'),
+      '#title' => $this->t('Desktop content'),
       '#default_value' => !empty($item->options['desktop']) ? $item->options['desktop'] : FALSE,
     ];
     $element['options']['tablet'] = [

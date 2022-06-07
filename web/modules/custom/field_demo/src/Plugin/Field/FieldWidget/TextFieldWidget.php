4e03aa18b1c269e6ea101d87c771144e76f74c78
Neha Gangwar <neha.gangwar@globant.com>
2022-06-07 18:34:16 +0530

    updating files

--- web/modules/custom/field_demo/src/Plugin/Field/FieldWidget/TextFieldWidget.php
+++ web/modules/custom/field_demo/src/Plugin/Field/FieldWidget/TextFieldWidget.php
@@ -25,20 +25,20 @@ class TextFieldWidget extends WidgetBase {
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     $item = $items[$delta];
 
-    $element['options']['desktop'] = [
-      '#type' => 'textfield',
+      $element['options']['desktop'] = [
+        '#type' => 'textfield',
       '#title' => $this->t('Desktop content'),
-      '#default_value' => !empty($item->options['desktop']) ? $item->options['desktop'] : FALSE,
+      '#default_value' => !empty($item->options['desktop']) ? $item->options['desktop'] : false,
     ];
     $element['options']['tablet'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Tablet content'),
-      '#default_value' => !empty($item->options['tablet']) ? $item->options['tablet'] : FALSE,
+      '#default_value' => !empty($item->options['tablet']) ? $item->options['tablet'] : false,
     ];
     $element['options']['mobile'] = [
-      '#type' => 'textfield',
+       '#type' => 'textfield',
       '#title' => $this->t('Mobile content'),
-      '#default_value' => !empty($item->options['mobile']) ? $item->options['mobile'] : FALSE,
+      '#default_value' => !empty($item->options['mobile']) ? $item->options['mobile'] : false,
     ];
 
     return $element;
